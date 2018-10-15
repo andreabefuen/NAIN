19610d3ec59a777bb90f68f00a2359809b4b1d47
axel8890 <axel8890@gmail.com>
2018-10-15 10:36:41 +0200

    Script Rehen

---
+++ IA-NAIN/Assets/Alex/Scripts/PlayerMovementAlex.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementAlex : MonoBehaviour {
+	private Rigidbody rb;
+	private float velocidad;
+	// Use this for initialization
+	void Awake () {
+		rb = GetComponent<Rigidbody> ();
+		velocidad = 0.5f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.A)) {
+			this.transform.Translate (Vector3.left * velocidad);
+		} if (Input.GetKey (KeyCode.D)) {
+			this.transform.Translate (Vector3.right * velocidad);
+		} if (Input.GetKey (KeyCode.W)) {
+			this.transform.Translate (Vector3.forward * velocidad);
+		} if (Input.GetKey (KeyCode.S)) {
+			this.transform.Translate (Vector3.back * velocidad);
+		} 
+		
+	}
+}
