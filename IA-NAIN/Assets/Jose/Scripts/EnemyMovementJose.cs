6b3816eea5e720ea85226b7d8518d20230c97990
al341552 <33596891+al341552@users.noreply.github.com>
2018-10-30 17:26:32 +0100

    Jose commit

---
+++ IA-NAIN/Assets/Jose/Scripts/EnemyMovementJose.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementJose : MonoBehaviour {
+
+	GameObject player;
+	float speed = 6f;
+	public bool ToPlayer;
+
+	void Start()
+	{
+		ToPlayer = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//define comportamiento del enemigo segun su tamaño
+		if (ToPlayer) {
+			if (Mathf.Abs (Vector3.Distance (this.transform.position, player.transform.position)) > 7) {
+				transform.LookAt (player.transform.position);
+				transform.Translate (new Vector3 (0, 0, speed * Time.deltaTime));
+			}
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			player = other.gameObject;
+			ToPlayer = true;
+		}
+
+	}
+}
---
+++ IA-NAIN/Assets/Jose/Scripts/SwitchDoorScriptJose.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchDoorScriptJose : MonoBehaviour {
+	
+	GameObject player;
+	GameObject door;
+	float speed = -1f;
+	float doorSpeed = 2.5f;
+	float switchDist;
+	float doorDist;
+	bool Pressed;
+
+	// Use this for initialization
+	void Start () {
+		Pressed = false;
+		door = GameObject.Find ("Door");
+		switchDist = this.transform.position.x - this.transform.localScale.x + 0.3f;
+		doorDist = door.transform.position.z + door.transform.localScale.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Pressed) {
+			if (Mathf.Abs(Vector3.Distance(this.transform.position, player.transform.position)) > 2)
+			{
+				
+				if (this.transform.position.x >= switchDist) {
+					transform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
+
+				}
+			}
+
+			if(this.transform.position.x <= switchDist){
+				if(door.transform.position.z <= doorDist){
+					door.transform.Translate(new Vector3 (0, 0, doorSpeed * Time.deltaTime));
+				}
+			}
+		}
+	}
+
+	private void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player")
+		{
+			player = other.gameObject;
+			Pressed = true;
+		}
+
+	}
+
+
+}
