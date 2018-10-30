ef4604aa0800e1e69d6d89087316554b92ef30bf
axel8890 <axel8890@gmail.com>
2018-10-30 09:20:44 +0100

    Timer

---
+++ IA-NAIN/Assets/Alex/Scripts/GameManagerAxel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerAxel : MonoBehaviour {
+
+    float timer;
+    Text TimeText;
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+        TimeText = GameObject.Find("TimerText").GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timer += Time.deltaTime;
+        int seconds = (int)timer % 60;
+        int min = (int)timer / 60;
+        TimeText.text = "Time: " + min.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
