44d3c3f82e151beaac98d6adf8643916046935c0
andreabefuen <al341488@uji.es>
2018-11-01 18:22:09 +0100

    Menu y Animaciones

---
+++ IA NEW/Assets/Scripts/MenuBottonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBottonManager : MonoBehaviour
+{
+
+    public string FirstLevel;
+
+
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene(FirstLevel);
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+
+}
