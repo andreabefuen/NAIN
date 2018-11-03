e84b1a7163493614f2ee20d92a98dd2d1c5a775e
andreabefuen <al341488@uji.es>
2018-11-03 19:45:42 +0100

    El enemigo ya pega cuando te encuentra, y dispara
    Hasta la pollísima haced algo ya xD

---
+++ IA NEW/Assets/Scripts/Guard.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+	//Andrea
+    public bool gizmosActivate;
+    public Transform pathHolder;
+
+    public float speed = 5;
+    public float waitTime = 0.3f;
+
+	//Marcos
+	private EnemyScriptMarcos detectThis;
+
+    private void Start()
+    {
+		//Marcos
+		detectThis = this.GetComponent<EnemyScriptMarcos> ();
+
+		//Andrea
+        Vector3[] waypoints = new Vector3[pathHolder.childCount];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = pathHolder.GetChild(i).position;
+            waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
+        }
+
+        StartCoroutine(FollowPath(waypoints));
+
+    }
+
+    IEnumerator FollowPath(Vector3[] waypoints)
+    {
+        transform.position = waypoints[0];
+
+        int targetWaypointIndex = 1;
+        Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+
+        while (true)
+        {
+			//If escrito por Marcos y else lo que habia originalmente de Andrea en el bucle
+			if (detectThis.GetPursuit ()) {
+				yield return null;
+			}
+			else{
+	            transform.LookAt(targetWaypoint);
+	            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
+	            if (transform.position == targetWaypoint)
+	            {
+	                targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length; //Para reiniciar el contador de waypoints
+	                targetWaypoint = waypoints[targetWaypointIndex];
+	                targetWaypoint = waypoints[targetWaypointIndex];
+	                yield return new WaitForSeconds(waitTime);
+	            }
+	            yield return null;
+			}
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (gizmosActivate)
+        {
+            Vector3 startPosition = pathHolder.GetChild(0).position;
+            Vector3 previousPosition = startPosition;
+            foreach (Transform waypoint in pathHolder)
+            {
+                Gizmos.DrawSphere(waypoint.position, 3f);
+                Gizmos.DrawLine(previousPosition, waypoint.position);
+                previousPosition = waypoint.position;
+            }
+            Gizmos.DrawLine(previousPosition, startPosition);
+        }
+
+    }
+}
---
+++ IA NEW/Assets/Scripts/PathFollowing.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollowing : MonoBehaviour {
+
+   // public Grid grid;
+    Pathfinding pathfinding;
+
+    public float reachDistance;
+
+    public bool isShooter;
+    public  float shootDistance;
+
+    public Transform target; //target of this enemy
+    public float speed;
+    Vector3[] path;
+    int targetIndex;
+
+    Rigidbody enemyRigidbody;
+    Vector3 movement;
+
+    List<Nodo> pathToFollow;
+
+    Queue<Nodo> queuePath;
+    Nodo newNodo;
+
+    Animator anim;
+
+    
+    bool reachDestination = false;
+
+    float initialSpeed;
+
+
+
+    private void Awake()
+    {
+        
+    }
+
+    
+    // Use this for initialization
+    void Start () {
+
+       
+
+        anim = GetComponent<Animator>();
+        //
+        // anim.SetBool("isWatching", true);
+        // anim.SetBool("isFar", true);
+        //
+     
+        enemyRigidbody = GetComponent<Rigidbody>();
+
+        pathfinding = GetComponent<Pathfinding>();
+
+        queuePath = new Queue<Nodo>();
+
+        initialSpeed = speed;
+
+       Invoke("PathFollowingTo", 5);
+	}
+
+    void PathFollowingTo()
+    {
+
+        //Debug.Log("Se inicia");
+
+
+        pathToFollow = pathfinding.pathPublic;
+        //pathToFollow.Reverse();
+
+        queuePath.Clear();
+        reachDestination = false;
+
+
+        foreach(Nodo n in pathToFollow)
+        {
+            queuePath.Enqueue(n);
+        }
+
+        WalkToANode(queuePath.Dequeue());
+
+    }
+
+
+    bool WalkToANode(Nodo n)
+    {
+
+        Vector3 dest;
+        Vector3 offset;
+        
+
+        dest = n.worldPosition;
+        offset = dest - transform.position;
+
+       // Debug.Log("magnitud :" + offset.magnitude);
+
+        if(offset.magnitude > reachDistance)
+        {
+
+            if(isShooter)
+            {
+                Vector3 dstToPlayer;
+                Vector3 aux;
+
+                dstToPlayer = target.position;
+                aux = dstToPlayer - transform.position;
+
+                if(aux.magnitude <= shootDistance)
+                {
+                    anim.SetBool("isWatching", true);
+                    anim.SetBool("isFar", false);
+                    anim.SetTrigger("shoot");
+                    Debug.Log("DISPARA");
+                    speed = 0;
+                }
+                else
+                {
+                    anim.SetBool("isWatching", true);
+                    anim.SetBool("isFar", true);
+                    speed = initialSpeed;
+                }
+              
+
+                
+            }
+            else
+            {
+                anim.SetBool("isWatching", true);
+                anim.SetBool("isFar", true);
+                speed = initialSpeed;
+            }
+
+             offset = offset.normalized;
+
+             
+             transform.LookAt(target);
+             transform.Translate(offset * speed * Time.deltaTime, Space.World);
+
+             return false;
+            
+
+
+           
+
+            
+        }
+        else
+        {
+           
+            return true;
+        }
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        
+
+        if(queuePath.Count > 0)
+        {
+            newNodo = queuePath.Peek();
+            
+           // Debug.Log("Elemento en cola: " + queuePath.Count);
+
+            if (WalkToANode(newNodo))
+            {
+                queuePath.Dequeue();
+            }
+        }
+        else
+        {
+            //Debug.Log("hA LLEGADO");
+            reachDestination = true;
+            anim.SetBool("isWatching", true);
+            anim.SetBool("isFar", false);
+            anim.SetTrigger("punch");
+           
+
+        }
+
+        Invoke("PathFollowingTo", 1);
+        
+        
+
+    }
+}
