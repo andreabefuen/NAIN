31bd07a3a0631c3e6a4bea03cb53f62df3430c13
axel8890 <axel8890@gmail.com>
2018-11-04 17:31:43 +0100

    Cambios

---
+++ IA NEW/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    float timer;
+    public Text TimeText;
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        int seconds = (int)timer % 60;
+        int min = (int)timer / 60;
+        TimeText.text = "Time: " + min.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
